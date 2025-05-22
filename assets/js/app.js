6b80123749183b7964d77229983bac3626b49c49
shubham <shubham@stellaripl.com>
2025-05-22 18:22:48 +0530

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const mainContent = document.getElementById('mainContent');
 
-let baseUrl = `https://api.github.com/users/`;
+const baseUrl = `https://api.github.com/users/`;
 
 let gitArr = [];
 
@@ -40,64 +40,59 @@ const gitTemplating = (arr) => {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    gitArr = [];
+    mainContent.innerHTML = '';
+
     let gitUrl = `${baseUrl}${search.value}`;
-    let reposUrl = ` ${baseUrl}${search.value}/repos?sort=created`;
+    let reposUrl = `${baseUrl}${search.value}/repos?sort=created`;
 
     const makeAPiCall = (method, apiUrl, body) => {
         return new Promise((resolve, reject) => {
             let xhr = new XMLHttpRequest;
             xhr.open(method, apiUrl);
             xhr.onload = () => {
-                xhr.status >= 200 || xhr.status < 300 ? resolve(xhr.response) : reject('Something went wrong')
+                xhr.status >= 200 && xhr.status < 300 ? resolve(xhr.response) : reject('Something went wrong');
             }
-            xhr.send(body)
-        })
-    }
+            xhr.send(body);
+        });
+    };
+
     makeAPiCall('GET', gitUrl)
         .then((res) => {
             let data = JSON.parse(res);
-            gitArr.push(data)
-            gitTemplating(gitArr)
-            cl(gitArr)
-            return makeAPiCall('GET', reposUrl)
+            gitArr.push(data);
+            gitTemplating(gitArr);
+            cl(gitArr);
+            return makeAPiCall('GET', reposUrl);
         })
         .then((res) => {
             let reposData = JSON.parse(res);
             let arr = [...reposData];
-            let fiveRepos = arr.slice(0,5)
-            cl(fiveRepos)
-            const ReposList = document.getElementById('ReposList')
+            let fiveRepos = arr.slice(0, 5);
+            cl(fiveRepos);
+
+            const allCards = document.querySelectorAll('.card');
+            const latestCard = allCards[allCards.length - 1];
+            const ReposList = latestCard.querySelector('#ReposList');
+
             let ul = document.createElement('ul');
             ul.className = `Repos d-flex p-0`;
-            let result = 
-            `
-                <li>
-                    <a href="${fiveRepos[0].html_url}" target="_blank">${fiveRepos[0].name}</a>
-                </li>
-                <li>
-                    <a href="${fiveRepos[1].html_url}" target="_blank">${fiveRepos[1].name}</a>
-                </li>
-                <li>
-                    <a href="${fiveRepos[2].html_url}" target="_blank">${fiveRepos[2].name}</a>
-                </li>
-                <li>
-                    <a href="${fiveRepos[3].html_url}" target="_blank">${fiveRepos[3].name}</a>
-                </li>
-                <li>
-                    <a href="${fiveRepos[4].html_url}" target="_blank">${fiveRepos[4].name}</a>
-                </li>
-            `
+
+            let result = '';
+            fiveRepos.forEach(repo => {
+                result += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
+            });
+
             ul.innerHTML = result;
-            ReposList.append(ul)
+            ReposList.append(ul);
         })
         .catch((err) => {
-            cl(err)
+            cl(err);
+            mainContent.innerHTML = `<div class="alert alert-danger mt-3">${err}</div>`;
         })
         .finally(() => {
             e.target.reset();
-        })
-
-
-})
+        });
+});
 
  
